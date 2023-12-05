461ea0caa0b998dce08c9e23cdc27278f800cdb5
MaxUAforever <maxuaforever@gmail.com>
2023-12-05 12:57:20 +0200

    [ST-053] Add movement and aiming animations

--- Source/SeriousTank/Private/Actors/Characters/Soldiers/ST_BaseSoldierAnimInstance.cpp
+++ Source/SeriousTank/Private/Actors/Characters/Soldiers/ST_BaseSoldierAnimInstance.cpp
@@ -1,6 +1,8 @@
 #include "Actors/Characters/Soldiers/ST_BaseSoldierAnimInstance.h"
 
+#include "Actors/Characters/Soldiers/ST_BaseSoldierCharacter.h"
 #include "Components/ST_SoldierMovementComponent.h"
+#include "Components/Weapons/ST_BaseWeaponsManagerComponent.h"
 #include "Core/ST_CoreTypes.h"
 #include "GameFramework/Character.h"
 #include "Kismet/KismetMathLibrary.h"
@@ -10,12 +12,60 @@ void UST_BaseSoldierAnimInstance::NativeInitializeAnimation()
 	Super::NativeInitializeAnimation();
 
 	MovingType = ECharacterMovingType::Standing;
-
-	if (ACharacter* OwningCharacter = Cast<ACharacter>(TryGetPawnOwner()))
+	SoldierCharacter = Cast<AST_BaseSoldierCharacter>(TryGetPawnOwner());
+	if (!SoldierCharacter)
 	{
-		if (UST_SoldierMovementComponent* MovementComponent = Cast<UST_SoldierMovementComponent>(OwningCharacter->GetMovementComponent()))
+		return;
+	}
+
+	if (UST_SoldierMovementComponent* MovementComponent = Cast<UST_SoldierMovementComponent>(SoldierCharacter->GetMovementComponent()))
+	{
+		MovementComponent->OnMovingTypeChanged.BindUObject(this, &ThisClass::OnMovementTypeChanged);
+	}
+	
+	TArray<UActorComponent*> WeaponManagerComponents;
+	SoldierCharacter->GetComponents(UST_BaseWeaponsManagerComponent::StaticClass(), WeaponManagerComponents);
+	if (WeaponManagerComponents.Num() > 0)
+	{
+		if (UST_BaseWeaponsManagerComponent* WeaponManagerComponent = Cast<UST_BaseWeaponsManagerComponent>(WeaponManagerComponents[0]))
 		{
-			MovementComponent->OnMovingTypeChanged.BindUObject(this, &ThisClass::OnMovementTypeChanged);
+			WeaponManagerComponent->OnWeaponAdded.AddUObject(this, &ThisClass::OnWeaponEquipped);
 		}
 	}
+}
+
+void UST_BaseSoldierAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (!SoldierCharacter || !SoldierCharacter->GetMesh())
+	{
+		return;
+	}
+		
+	const float CameraYaw = SoldierCharacter->GetCameraYawAngle();
+		
+	const bool bIsMoving = !SoldierCharacter->GetVelocity().IsNearlyZero();
+	if (bIsMoving)
+	{
+		const float VelocityYaw = UKismetMathLibrary::MakeRotFromX(SoldierCharacter->GetVelocity()).Yaw;
+		YawWalkOffset = VelocityYaw - CameraYaw;
+		YawAimOffset = 0.f;
+	}
+	else
+	{
+		const float RootYaw = SoldierCharacter->GetRootComponent()->GetComponentRotation().Yaw;
+		YawAimOffset = CameraYaw - RootYaw;
+		YawWalkOffset = 0.f;
+
+		if (FMath::Abs(YawAimOffset) > MaxYawAimOffset)
+		{
+			OnMaxAimAngleReached.ExecuteIfBound(YawAimOffset);
+		}
+	}
+}
+
+void UST_BaseSoldierAnimInstance::OnWeaponEquipped(int32 WeaponIndex, AST_BaseWeapon* Weapon)
+{
+	bIsWeaponEquipped = true;
 }
--- Source/SeriousTank/Public/Actors/Characters/Soldiers/ST_BaseSoldierAnimInstance.h
+++ Source/SeriousTank/Public/Actors/Characters/Soldiers/ST_BaseSoldierAnimInstance.h
@@ -3,20 +3,45 @@
 #include "Animation/AnimInstance.h"
 #include "ST_BaseSoldierAnimInstance.generated.h"
 
+class AST_BaseSoldierCharacter;
+class AST_BaseWeapon;
 enum class ECharacterMovingType : uint8;
+
+DECLARE_DELEGATE_OneParam(FOnMaxAimAngleReached, float);
 
 UCLASS()
 class SERIOUSTANK_API UST_BaseSoldierAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	FOnMaxAimAngleReached OnMaxAimAngleReached;
+
 protected:
 	UPROPERTY(BlueprintReadOnly)
 	ECharacterMovingType MovingType;
 
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsWeaponEquipped;
+
+	UPROPERTY(BlueprintReadOnly)
+	float YawWalkOffset;
+
+	UPROPERTY(BlueprintReadOnly)
+	float YawAimOffset;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxYawAimOffset;
+
+private:
+	AST_BaseSoldierCharacter* SoldierCharacter;
+
 public:
 	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
 
 private:
 	FORCEINLINE void OnMovementTypeChanged(ECharacterMovingType InMovingType) { MovingType = InMovingType; };
+
+	void OnWeaponEquipped(int32 WeaponIndex, AST_BaseWeapon* Weapon);
 };
